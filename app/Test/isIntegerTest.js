fa39e75e6994c2459ce0642c35967d28746ab2a3
方娄昊 <825064728@qq.com>
2017-11-20 19:40:52 +0800

    使用karma进行单元测试，但在es6转换成es5时出现问题，出现Uncaught SyntaxError: Unexpected token import报错

---
+++ app/Test/isIntegerTest.js
@@ -0,0 +1,9 @@
+import {isNum} from '../src/isInteger.js'
+
+describe("This is a integer test!", function(){
+    it("Is integer", function() {
+        expect(true).toEqual(isInteger(20));
+        expect(false).toEqual(isInteger("20"));
+        expect(false).toEqual(isInteger(0));
+    })
+});
---
+++ app/src/isInteger.js
@@ -0,0 +1,5 @@
+function isInteger(num) {
+    if(typeof num !=="number") return false;
+    var pattern = /^[1-9]\d*$/g;
+    return pattern.test(num);
+}
